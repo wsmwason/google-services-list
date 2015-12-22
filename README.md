692190418f81921dc3a0ee1e298ff503349392ad
wsmwason <wsmwason@gmail.com>
2015-12-22 16:34:00 +0800

    Add google-services-list.user.js

---
+++ README.md
@@ -0,0 +1,22 @@
+Google Services List
+===
+
+List all Google services on support page, and auto redirect to product url.
+
+### About
+
+Google has many service but no list all services in one page, 
+Here use the [Google Help Center](https://support.google.com/) have all service help,
+Helper Center include Consumer/Business/Developer category all full list.
+When enabled this script, all service help page will open new tab and auto redirect to product url. Enjoy!
+
+### Features
+
+  * List all Google service on [Google Help Center](https://support.google.com/)
+  * Auto show more service and hide button
+  * Click service help open new tab and redirect to product url
+
+### History
+
+    2015/12/22 v1.0
+    -- Release v1.0
---
+++ google-services-list.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name        Google Service List
+// @namespace   wsmwason.google.service.list
+// @description List all Google services on support page, and auto redirect to product url.
+// @include     https://support.google.com/*
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @auther      wsmwason
+// @version     1.0
+// @license     MIT
+// @grant       none
+// ==/UserScript==
+
+// hide show all icon
+$('.show-all').hide();
+
+// display hide container
+$('.all-hc-container').addClass('all-hc-container-shown');
+
+// add link redirect param for next page
+$('a').each(function(){
+    var stid = $(this).attr('st-id');
+    if (typeof stid !== typeof undefined && stid !== false) {
+        $(this).attr('href', $(this).attr('href')+'&redirect=1').attr('target', '_blank');
+    }
+});
+
+// auto redirect to product url
+if (location.search.indexOf('redirect=1') > 0) {
+    // find product-icon link
+    var productIcon = $('a.product-icon');
+    if (productIcon.length == 1) {
+        var productUrl = productIcon.attr('href');
+        location.href = productUrl;
+    }
+}
